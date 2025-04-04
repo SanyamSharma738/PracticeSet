a6a5961dd16c3d7d022eaa476f7885ba70fd7391
SanyamSharma738 <sanyamsharma081981@gmail.com>
2025-04-04 11:24:09 +0530

    InterfaceExample.java
    trying multiple inheritance using interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+//Interfaces example to see multiple inheritance
+interface Swimable{
+    void swim();
+}
+interface Flyable{
+    void fly();
+}
+class Shark implements Swimable{
+    @Override
+    public void swim() {
+        System.out.println("Shark is Swimming");
+    }
+}
+class Eagle implements Flyable{
+    @Override
+    public void fly() {
+        System.out.println("Eagle is Flying");
+    }
+}
+class Duck implements Swimable,Flyable{
+    @Override
+    public void swim() {
+        System.out.println("Duck can Swim");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Duck can Fly");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Shark sh = new Shark();
+        sh.swim();
+        Eagle egl = new Eagle();
+        egl.fly();
+        Duck dc = new Duck();
+        dc.swim();
+        dc.fly();
     }
 }
